ea18b18b30e91c0a0ba1aa7c7f7378608937eac6
marijajoveva <jovevamarija9@gmail.com>
2026-01-18 17:26:47 +0100

    Ready version with Favorites feature and updated README

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,3 @@
-// import 'package:flutter/material.dart';
-// import 'screens/home_screen.dart';
-//
-// void main() => runApp(MyApp());
-//
-// class MyApp extends StatelessWidget {
-//   const MyApp({super.key});
-//
-//   @override
-//   Widget build(BuildContext context) {
-//     return MaterialApp(
-//       title: 'Recipes App',
-//       theme: ThemeData(primarySwatch: Colors.teal),
-//       home: HomeScreen(),
-//     );
-//   }
-// }
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'screens/home_screen.dart';
